e39f7b9049ba2b44ae8c816cf739237e3ea81015
jurajvandor <433451@mail.muni.cz>
2017-02-13 22:29:27 +0100

    work

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,26 +1,12 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Player extends CardOwner {
 
 	private Map<String, String> properties;
 
-	/**
-	 * 
-	 * @param deck
-	 */
-	public void drawCard(Deck deck) {
-		// TODO - implement Player.drawCard
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param card
-	 * @param deck
-	 */
-	public boolean discardFromHand(Card card, Deck deck) {
-		// TODO - implement Player.discardFromHand
-		throw new UnsupportedOperationException();
+	public Player() {
+		this.properties = new HashMap<String, String>();
 	}
 
 	/**
