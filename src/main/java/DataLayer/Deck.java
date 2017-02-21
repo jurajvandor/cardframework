a10c80a5023d9e34c0c63b48039f6bdaaf956d1a
jurajvandor <433451@mail.muni.cz>
2017-02-21 02:33:41 +0100

    work

--- src/main/java/DataLayer/Deck.java
+++ src/main/java/DataLayer/Deck.java
@@ -1,10 +1,7 @@
 package DataLayer;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Deck implements GroupOfCards, Serializable {
 
@@ -16,15 +13,23 @@ public class Deck implements GroupOfCards, Serializable {
 		this.cards = cards;
 	}
 	public Card drawTopCard() {
-		return cards.get(cards.size());
+		return cards.remove(cards.size());
 	}
 
 	public void returnCardToTop(Card card) {
-
+		cards.add(card);
 	}
-
+/*for i from 0 to n−2 do
+	j ← random integer such that i ≤ j < n
+	exchange a[i] and a[j]
+	https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle */
 	public void shuffle() {
-
+		for (int i = 0; i < cards.size()-2; i++){
+			int j = Dice.roll(i,cards.size()-1);
+			Card tmp = cards.get(i);
+			cards.set(i, cards.get(j));
+			cards.set(j, tmp);
+		}
 	}
 
 	public DeckType getType() {
@@ -36,7 +41,7 @@ public class Deck implements GroupOfCards, Serializable {
 	}
 
 	public Collection<Card> getCards() {
-		return cards;
+		return Collections.unmodifiableList(cards);
 	}
 
 	public void addCard(Card card) {
