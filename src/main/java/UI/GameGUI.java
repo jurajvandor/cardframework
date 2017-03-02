fdd869eb3ae3588a384dedd38f159f687d71a2fc
jurajvandor <433451@mail.muni.cz>
2017-03-02 16:49:09 +0100

    work

---
+++ src/main/java/UI/GameGUI.java
@@ -0,0 +1,18 @@
+package UI;/**
+ * Created by Juraj on 02.03.2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/main/java/UI/ServerUI.java
@@ -0,0 +1,7 @@
+package UI;
+
+/**
+ * Created by Juraj on 02.03.2017.
+ */
+public class ServerUI {
+}
