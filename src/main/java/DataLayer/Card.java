de44b87ad82b96160f14b536fc556882d391498b
jurajvandor <433451@mail.muni.cz>
2017-04-04 00:23:15 +0200

    comments

--- src/main/java/DataLayer/Card.java
+++ src/main/java/DataLayer/Card.java
@@ -9,22 +9,43 @@ import java.util.Map;
 /**
  * Created by Juraj Vandor on 14.03.2017.
  */
+
+/**
+ * represents single playing card with its properties stored in map with String key and String value
+ */
 public class Card implements Serializable {
 
 	private HashMap<String, String> properties;
 
-	public Card(HashMap<String,String> properties){
-		this.properties = properties;
+	/**
+	 * creates card with given properties
+	 * @param properties properties of card (they are beeing copied)
+	 */
+	public Card(Map<String,String> properties){
+		if (properties == null)
+			throw new NullPointerException("properties is null");
+		this.properties = new HashMap<>(properties);
 	}
 
+	/**
+	 * copies card properties
+	 * @param card card with the same properties
+	 */
 	public Card(Card card){
 		this.properties = new HashMap<>(card.properties);
 	}
 
+	/**
+	 * @param name key - type of property (health, attack, suit, value, etc..)
+	 * @return property of card for given type, null if key does not exist
+	 */
 	public String getProperty(String name){
 		return properties.get(name);
 	}
 
+	/**
+	 * @return properties for cared in unmodifiable Map
+	 */
 	public Map<String, String> getProperties() {
 		return Collections.unmodifiableMap(this.properties);
 	}
--- src/main/java/DataLayer/CardOwner.java
+++ src/main/java/DataLayer/CardOwner.java
@@ -6,17 +6,42 @@ import java.util.Map;
 /**
  * Created by Juraj Vandor on 14.03.2017.
  */
+
+/**
+ * represents card owner (in basic version of this framework can be Desk or Player
+ */
 public class CardOwner  implements Serializable {
 
     private HashMap<String, GroupOfCards> cards;
 
+    /**
+     * creates map of groups of cards with string (name) as a key
+     */
     public CardOwner(){
-        cards = new HashMap<String, GroupOfCards>();
+        cards = new HashMap<>();
     }
+
+    /**
+     * copies map of groups of cards
+     * @param cards map that stores groups of cards with name as a key
+     */
+    public CardOwner(Map<String, GroupOfCards> cards){
+        cards = new HashMap<>(cards);
+    }
+
+    /**
+     * adds group of cards with given name to owner
+     * @param name key to be stored with (must be unique or value is rewritten)
+     * @param cards group of cards to be stored
+     */
     public void addCards(String name, GroupOfCards cards){
         this.cards.put(name, cards);
     }
 
+    /**
+     * @param name key under which group of cards is stored
+     * @return group of cards under given name (could be null if key does not exit)
+     */
     public GroupOfCards getCards(String name){
         return cards.get(name);
     }
